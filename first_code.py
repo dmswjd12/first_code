8fd26d0871d2b5b382d12986223b8cdf4459961b
dmswjd12 <dmswjdwjddjs@naver.com>
2023-10-11 16:28:57 +0900

    [master] 브랜치를[feature/cart] 브랜치에 병합 하려고 한다.

--- first_code.py
+++ first_code.py
@@ -1,12 +1,7 @@
 print("hello world")
 print("This is the first code")
 
-total = 0
-
-for i in range(1, 11):
-    total += i 
-
-print("1부터 10까지의 합:", total)
+print("Developer B")
 
 def calc_area(x,y):
     result = x * y
